aa18cbc3a01d2fc05f688035139e97ef15d73c22
Hung Pham <tanhunghue@gmail.com>
2020-06-11 08:47:13 +0700

    First API

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 https://realpython.com/flask-by-example-part-3-text-processing-with-requests-beautifulsoup-nltk/
+
+https://github.com/hack4impact/flask-base
+
+https://github.com/undertheseanlp/underthesea
--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from underthesea import sentiment
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 # app.config.from_object(os.environ['APP_SETTINGS'])
 
@@ -11,6 +13,16 @@ def hello():
 @app.route('/<name>')
 def name(name):
     return "Hello world {}".format(name)
+
+
+@app.route('/api/sentiment-analysis')
+def sentiments():
+    text = sentiment(
+        'hàng kém chất lg,chăn đắp lên dính lông lá khắp người. thất vọng')
+    print(text)
+    print(sentiment(
+        'Sản phẩm hơi nhỏ so với tưởng tượng nhưng chất lượng tốt, đóng gói cẩn thận.'))
+    return "Check Log"
 
 
 if __name__ == '__main__':
